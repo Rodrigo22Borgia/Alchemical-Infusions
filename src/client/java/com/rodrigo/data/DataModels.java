44e086a24a75d37b0e8a2b1a4401b4d20db1b7ea
Rodrigo22Borgia <vojta.vesecky@seznam.cz>
2025-11-02 15:18:49 +0100

    Multipart datagen setup

--- src/client/java/com/rodrigo/data/DataModels.java
+++ src/client/java/com/rodrigo/data/DataModels.java
@@ -1,17 +1,23 @@
 package com.rodrigo.data;
 
+import com.rodrigo.AlchemicalInfusions;
+import com.rodrigo.blocks.AlchemyBlock;
 import com.rodrigo.blocks._BlockRegistry;
 import net.fabricmc.fabric.api.client.datagen.v1.provider.FabricModelProvider;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
-import net.minecraft.client.data.BlockStateModelGenerator;
-import net.minecraft.client.data.ItemModelGenerator;
-import net.minecraft.client.data.ModelIds;
+import net.minecraft.client.data.*;
+import net.minecraft.client.render.model.json.MultipartModelConditionBuilder;
+import net.minecraft.registry.Registries;
+import net.minecraft.state.property.Property;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.Pair;
+import oshi.util.tuples.Pair;
+import oshi.util.tuples.Quartet;
 
-import static net.minecraft.client.data.BlockStateModelGenerator.createSingletonBlockState;
+import java.util.Optional;
+import java.util.function.Function;
+
+import static net.minecraft.client.data.BlockStateModelGenerator.createWeightedVariant;
 
 public class DataModels extends FabricModelProvider {
     public DataModels(FabricDataOutput output) {
@@ -19,16 +25,84 @@ public class DataModels extends FabricModelProvider {
     }
 
     @Override
-    public void generateBlockStateModels(BlockStateModelGenerator blockStateModelGenerator) {
-        //blockStateModelGenerator.registerStateWithModelReference(_BlockRegistry.alchemy, Blocks.BARRIER);
-        blockStateModelGenerator.registerSimpleState(_BlockRegistry.alchemy);
+    public void generateBlockStateModels(BlockStateModelGenerator generator) {
+        //generator.registerSimpleState(_BlockRegistry.alchemy);
+
+        Function<MultipartModelConditionBuilder, MultipartModelConditionBuilder> POTION1 = (builder) -> builder;
+        Function<MultipartModelConditionBuilder, MultipartModelConditionBuilder> POTION2 = (builder) -> builder;
+        Function<MultipartModelConditionBuilder, MultipartModelConditionBuilder> BREW = (builder) -> builder;
+        Function<MultipartModelConditionBuilder, MultipartModelConditionBuilder> FUEL = (builder) -> builder;
+
+        TexturedModel.Factory TEMPLATE_LEAF_LITTER_1 =  TexturedModel.makeFactory(TextureMap::texture, block("cube_all", TextureKey.of("wtf", TextureKey.ALL)));
+
+//        TextureKey t = TextureKey.of("zrdz");
+//        generator.blockStateCollector.accept(MultipartBlockModelDefinitionCreator.create(_BlockRegistry.alchemy)
+//                        .with(createWeightedVariant(texturedModelFactory("block").upload(_BlockRegistry.alchemy, generator.modelCollector)))
+//                .with((POTION1).apply(BlockStateModelGenerator.createMultipartConditionBuilder().put(AlchemyBlock.SLOT1, true)),
+//                        createWeightedVariant(block("block", t).upload(Identifier.of(AlchemicalInfusions.modid, "donno"), new TextureMap().put(t,Identifier.of("aaa")), generator.modelCollector)))
+//                .with(POTION1.apply(BlockStateModelGenerator.createMultipartConditionBuilder().put(AlchemyBlock.SLOT2, true)),
+//                        createWeightedVariant(texturedModelFactory("block").upload(_BlockRegistry.alchemy,"_slot2", generator.modelCollector)))
+//                .with(POTION1.apply(BlockStateModelGenerator.createMultipartConditionBuilder().put(AlchemyBlock.BREW, true)),
+//                        createWeightedVariant(texturedModelFactory("block").upload(_BlockRegistry.alchemy,"_brew", generator.modelCollector)))
+//                .with(POTION1.apply(BlockStateModelGenerator.createMultipartConditionBuilder().put(AlchemyBlock.FUEL, true)),
+//                        createWeightedVariant(texturedModelFactory("block").upload(_BlockRegistry.alchemy,"_fuel", generator.modelCollector))));
+
+
+        multipart(generator, _BlockRegistry.alchemy, Identifier.of(AlchemicalInfusions.modid, "block/alchemy"), new Pair[]{}, new Quartet[]{
+                new Quartet<>(Identifier.of(AlchemicalInfusions.modid, "block/alchemy_slot1"), AlchemyBlock.SLOT1, true, new Pair[]{}),
+                new Quartet<>(Identifier.of(AlchemicalInfusions.modid, "block/alchemy_slot2"), AlchemyBlock.SLOT2, true, new Pair[]{}),
+                new Quartet<>(Identifier.of(AlchemicalInfusions.modid, "block/alchemy_brew" ), AlchemyBlock.BREW , true, new Pair[]{}),
+                new Quartet<>(Identifier.of(AlchemicalInfusions.modid, "block/alchemy_fuel" ), AlchemyBlock.FUEL , true, new Pair[]{})
+        });
+
+
     }
-//
-//    public static void singleton(BlockStateModelGenerator G, Block block, String id, Identifier model, Identifier texture) {
-//        applyTextures(G, id, model, new Pair<>("0", texture));
-//        G.blockStateCollector.accept(createSingletonBlockState(block, Identifier.of(modid, id)));
-//        G.registerParentedItemModel(block, Identifier.of(modid, id));
-//    }
+    private static <T extends Comparable<T>> void multipart(BlockStateModelGenerator generator, Block block, Identifier parent, Pair<TextureKey, Identifier>[] main, Quartet<Identifier, Property<T>, T, Pair<TextureKey, Identifier>[]>[] parts) {
+        Function<MultipartModelConditionBuilder, MultipartModelConditionBuilder> BUILDER = (builder) -> builder;
+        String id = id(block);
+        int i, j = 0;
+
+        TextureMap textures = new TextureMap();
+        TextureKey[] keys = new TextureKey[main.length];
+
+        i = 0;
+        for (final Pair<TextureKey, Identifier> pairs : main) {
+            textures.put(pairs.getA(), pairs.getB());
+            keys[i++] = pairs.getA();
+        }
+
+        MultipartBlockModelDefinitionCreator creator = MultipartBlockModelDefinitionCreator.create(block);
+        creator.with(createWeightedVariant(new Model(Optional.of(parent), Optional.empty(), keys)
+                .upload(Identifier.of(AlchemicalInfusions.modid, id), textures, generator.modelCollector)));
+
+        for (Quartet<Identifier, Property<T>, T, Pair<TextureKey, Identifier>[]> quartet : parts) {
+            j++;
+            textures = new TextureMap();
+            keys = new TextureKey[quartet.getD().length];
+            i = 0;
+            for (final Pair<TextureKey, Identifier> pairs : quartet.getD()) {
+                textures.put(pairs.getA(), pairs.getB());
+                keys[i++] = pairs.getA();
+            }
+            creator.with(BUILDER.apply(BlockStateModelGenerator.createMultipartConditionBuilder().put(quartet.getB(), quartet.getC())),
+                    createWeightedVariant(new Model(Optional.of(quartet.getA()), Optional.empty(), keys)
+                            .upload(Identifier.of(AlchemicalInfusions.modid, id + "_" + j), textures, generator.modelCollector)));
+        }
+        generator.blockStateCollector.accept(creator);
+    }
+
+    public static String id(Block block) {
+        return Registries.BLOCK.getId(block).getPath().replaceFirst(".*\\.", "");
+    }
+
+    private static TexturedModel.Factory texturedModelFactory(String parent) {
+        return TexturedModel.makeFactory(TextureMap::texture, block(parent));
+        //Models;
+    }
+
+    private static Model block(String parent, TextureKey... requiredTextureKeys) {
+        return new Model(Optional.of(Identifier.of(AlchemicalInfusions.modid,"block/" + parent)), Optional.empty(), requiredTextureKeys);
+    }
 
     @Override
     public void generateItemModels(ItemModelGenerator itemModelGenerator) {
--- src/main/java/com/rodrigo/blocks/AlchemyBlock.java
+++ src/main/java/com/rodrigo/blocks/AlchemyBlock.java
@@ -1,6 +1,7 @@
 package com.rodrigo.blocks;
 
 import com.mojang.serialization.MapCodec;
+import com.rodrigo.AlchemicalInfusions;
 import com.rodrigo.entities.AlchemyEntity;
 import net.minecraft.block.*;
 import net.minecraft.block.entity.BlockEntity;
@@ -17,6 +18,7 @@ import net.minecraft.particle.ParticleTypes;
 import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.state.property.EnumProperty;
 import net.minecraft.state.property.Properties;
 import net.minecraft.util.ActionResult;
@@ -38,13 +40,17 @@ import java.util.*;
 import java.util.stream.Stream;
 
 public class AlchemyBlock extends BlockWithEntity  {
+    public static final BooleanProperty FUEL = BooleanProperty.of("fuel");
+    public static final BooleanProperty BREW = BooleanProperty.of("brew");
+    public static final BooleanProperty SLOT1= BooleanProperty.of("slot1");
+    public static final BooleanProperty SLOT2= BooleanProperty.of("slot2");
+    public static final BooleanProperty LIT  = Properties.LIT;
 
-    private static final VoxelShape[] shapes;
-    private static final EnumProperty<Direction> FACING = Properties.HORIZONTAL_FACING;
+    private static final VoxelShape shape;
 
     public AlchemyBlock(Settings settings){
         super(settings);
-        this.setDefaultState(getDefaultState().with(FACING, Direction.NORTH));
+        this.setDefaultState(getDefaultState().with(FUEL, false).with(BREW, false).with(SLOT1, false).with(SLOT2,false).with(LIT,false));
     }
 
     /**
@@ -61,35 +67,30 @@ public class AlchemyBlock extends BlockWithEntity  {
      */
     @Override
     public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, BlockHitResult hit) {
-        if (!(world.getBlockEntity(pos) instanceof final AlchemyEntity entity)) {return ActionResult.PASS;}
+        if (!(world.getBlockEntity(pos) instanceof final AlchemyEntity entity)) {
+            return ActionResult.PASS;
+        }
         if (player.getMainHandStack().getItem() == Items.FLINT_AND_STEEL) {
             return brewPotion(entity, world, pos, state);
         }
-        final Vec3d offset = pos.toCenterPos().subtract(hit.getPos());
 
-        int i = 3;
-        if (offset.y > 0)  {i = i - 2;}
-        if (state.get(Properties.HORIZONTAL_FACING).getAxis() == Direction.Axis.Z ?offset.x > 0 : offset.z > 0) {i = i - 1;}
-
-        final ItemStack inHand = player.getMainHandStack();
-        final Item  inHandItem = inHand.getItem();
-//        if (((i == 2 || i == 1) && !(inHandItem instanceof PotionItem || inHand.get(DataComponentTypes.POTION_CONTENTS) != null))
-//        || ((i == 3) && !(inHandItem == Items.SHORT_GRASS))
-//        || ((i == 0) && !(inHandItem instanceof PotionItem || inHandItem == Items.GLASS_BOTTLE))) {return ActionResult.PASS;}
-
-        if (entity.getStack(i).isEmpty() && !inHand.isEmpty()) {
-            entity.setStack(i, inHand);
-            player.setStackInHand(Hand.MAIN_HAND, ItemStack.EMPTY);
-            handleInteraction(entity, world, pos, state);
-            return ActionResult.SUCCESS;
-        } else if (!entity.getStack(i).isEmpty() && inHand.isEmpty()) {
-            player.setStackInHand(Hand.MAIN_HAND, entity.getStack(i));
-            entity.setStack(i, ItemStack.EMPTY);
-            handleInteraction(entity, world, pos, state);
-            return ActionResult.SUCCESS;
-        } else {
+        int i = switch ((int) (hit.getPos().y % 1 * 1000)) {
+            case 281 -> 0;
+            case 62 -> 1;
+            case 56 -> 2;
+            case 50 -> 3;
+            default -> 4;
+        };
+        AlchemicalInfusions.LOGGER.info(String.valueOf(hit.getPos().y % 1 * 1000));
+        if (i == 4) {
             return ActionResult.PASS;
         }
+
+
+        final ItemStack inHand = player.getMainHandStack();
+        player.setStackInHand(Hand.MAIN_HAND, entity.getStack(i));
+        entity.setStack(i, inHand);
+        return ActionResult.SUCCESS;
     }
     private ActionResult brewPotion(AlchemyEntity entity, World world, BlockPos pos, BlockState state) {
         final ItemStack fuel = entity.getStack(3);
@@ -207,17 +208,6 @@ public class AlchemyBlock extends BlockWithEntity  {
             }
         } else {return ActionResult.PASS;}
 
-
-
-//        slot0.applyComponentsFrom(ComponentMap.builder()
-//                .add(DataComponentTypes.CONSUMABLE, ConsumableComponent.builder().consumeEffect(new ApplyEffectsConsumeEffect(new StatusEffectInstance(StatusEffects.NIGHT_VISION, 3600))).build())
-//                .build());
-
-//        slot0.applyComponentsFrom(ComponentMap.builder()
-//                .add(DataComponentTypes.POTION_CONTENTS, PotionContentsComponent.DEFAULT
-//                        .with(new StatusEffectInstance(StatusEffects.NIGHT_VISION, 3600))
-//                ).build());
-
         entity.setStack(0, brew);
         fuel.decrement(1);
         handleInteraction(entity, world, pos, state);
@@ -260,11 +250,6 @@ public class AlchemyBlock extends BlockWithEntity  {
         }
     }
 
-    /**
-     * Vanilla constructors
-     * @return
-     */
-
     @Override
     protected MapCodec<? extends BlockWithEntity> getCodec() {
         return createCodec(AlchemyBlock::new);
@@ -272,45 +257,13 @@ public class AlchemyBlock extends BlockWithEntity  {
 
     @Override
     protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
-        builder.add(FACING);
-    }
-
-    @Override
-    protected BlockState rotate(BlockState state, BlockRotation rotation) {
-        return state.with(Properties.HORIZONTAL_FACING, rotation.rotate(state.get(Properties.HORIZONTAL_FACING)));
-    }
-
-    @Override
-    protected BlockState mirror(BlockState state, BlockMirror mirror) {
-        return state.with(Properties.HORIZONTAL_FACING, mirror.apply(state.get(Properties.HORIZONTAL_FACING)));
+        builder.add(BREW, FUEL, SLOT1, SLOT2, LIT);
     }
 
     @Override
     protected VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
-        switch (state.get(FACING)){
-            case NORTH -> {
-                return shapes[0];
-            }
-            case EAST -> {
-                return shapes[1];
-            }
-            case SOUTH -> {
-                return shapes[2];
-            }
-            case WEST -> {
-                return shapes[3];
-            }
-            default -> {
-                return VoxelShapes.fullCube();
-            }
-        }
+        return shape;
     }
-
-    @Override
-    public BlockState getPlacementState(ItemPlacementContext ctx) {
-        return getDefaultState().with(FACING, ctx.getHorizontalPlayerFacing().getOpposite());
-    }
-
 
     private static final HashMap<Item, StatusEffectInstance[]> INGREDIENT_MAP = new HashMap<>();
     static {
@@ -318,15 +271,11 @@ public class AlchemyBlock extends BlockWithEntity  {
         INGREDIENT_MAP.put(Items.SWEET_BERRIES, new StatusEffectInstance[]{new StatusEffectInstance(StatusEffects.SATURATION, 200), new StatusEffectInstance(StatusEffects.ABSORPTION, 200)});
         INGREDIENT_MAP.put(Items.GOLDEN_APPLE, new StatusEffectInstance[]{new StatusEffectInstance(StatusEffects.ABSORPTION, 500)});
 
-        shapes = new VoxelShape[4];
-        shapes[0] = VoxelShapes.union(
+        shape = VoxelShapes.union(
                 VoxelShapes.cuboid(0.0625, 0, 0.3125, 0.4375, 0.05625, 0.6875),
-                VoxelShapes.cuboid(0.5625, 0, 0.5625, 0.9375, 0.049999999999999996, 0.9375),
+                VoxelShapes.cuboid(0.5625, 0, 0.5625, 0.9375, 0.05, 0.9375),
                 VoxelShapes.cuboid(0.5, 0, 0.0625, 0.875, 0.0625, 0.4375),
                 VoxelShapes.cuboid(0.5625, 0.21875, 0.5625, 0.9375, 0.28125, 0.9375)
         );
-        shapes[1] = VoxelTransform.rotate90(shapes[0]);
-        shapes[2] = VoxelTransform.rotate180(shapes[0]);
-        shapes[3] = VoxelTransform.rotate270(shapes[0]);
     }
 }
